5f38d0ae844f320b5a675150c195381fed95ef3e
pharmashawarma <pharmashawarma@gmail.com>
2019-12-02 15:08:59 -0500

    Top module laid out

---
+++ src/whack_a_mole/whack_a_mole.srcs/sources_1/new/whack_a_mole_top.v
@@ -0,0 +1,44 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/02/2019 02:59:14 PM
+// Design Name: 
+// Module Name: whack_a_mole_top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module whack_a_mole_top(
+        input clk,
+        input reset
+    );
+    
+    // inititial countdown module
+    //      7 segment display
+    //          input - a bus containing what to display, output - none
+    //      5 second counter (should be the same as 30 second)
+    //          input - 5 and down count instruction, output - 1 for done
+    
+    // game module
+    //      30 second counter (should be the same as 5 second)
+    //          input - 30 and up count instruction, output - 1 for done
+    //      score tracker module
+    //          input - score as a bus, output - none
+    //          7 segment display (same as before)
+    //      randomizer
+    //          input - either random variable or self generate, output - bus containing state
+    //      mole 
+    //          input - bus containing lights turned on, output - hit or not
+endmodule
