20311f5b0d28bfd19f3374d54b6c404383401980
Li <liben002@bu.edu>
2019-12-04 17:59:50 -0500

    Commented modules

--- src/whack_a_mole/whack_a_mole.srcs/sources_1/new/whack_a_mole_top.v
+++ src/whack_a_mole/whack_a_mole.srcs/sources_1/new/whack_a_mole_top.v
@@ -33,12 +33,20 @@ module whack_a_mole_top(
     wire [3:0] character;
     wire [31:0] display;
     
+    // top level clock dividers, pass to each module that uses it
     clock_divider100MHzTo1kHz c_dMTokHz(.clk(clk), .reset(reset), .outClk(outClk_kHz));
     clock_divider100MHzTo1Hz c_dMToHz(.clk(clk), .reset(reset), .outClk(outClk_Hz));
     
+    // Input - top level clock, enable signal, reset
+    // Output - a 32 bit bus containing everything that will be displayed in binary (display)
     startup s0(.clk(clk), .enable(1), .reset(reset), .display(display));
     
+    // Input - 1 KHz clock, reset, a 32 bit bus containing everything that will be displayed in binary (display)
+    // Output - 8 bit bus for what segment should be used (ANLine), 4 bit bus containing what character should be used (character)
     display_control d_c(.clk(outClk_kHz), .reset(reset), .display(display), .ANLine(ANLine), .character(character));
+    
+    // Input - top level clock, reset, 4 bit bus containing what character should be used (character)
+    // Output - 8 bit bus containing what segments should be lit up (displaySegments)
     seven_segment_decoder s_7_d(.clk(clk), .reset(reset), .character(character), .displaySegments(displaySegments));
     // inititial countdown module - BEN DONE
     //      7 segment display - BEN DONE
