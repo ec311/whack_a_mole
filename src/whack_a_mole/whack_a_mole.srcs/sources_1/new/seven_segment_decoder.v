1695c7ded29a1332c48a0a2fbce5a9482b3c9196
Benjamin Li <pharmashawarma@gmail.com>
2019-12-04 12:56:51 -0500

    Added encoding for characters

---
+++ src/whack_a_mole/whack_a_mole.srcs/sources_1/new/seven_segment_decoder.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/04/2019 12:45:59 PM
+// Design Name: 
+// Module Name: seven_segment_decoder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module seven_segment_decoder(
+
+    );
+endmodule
